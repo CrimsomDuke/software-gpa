558b870b4e649c4189ff8d2a98cc30030607eac1
CrimsomDuke <grottordrynor@gmail.com>
2025-05-10 08:51:19 -0400

    cambios

--- gpa_backend/config/global.js
+++ gpa_backend/config/global.js
@@ -1,6 +1,7 @@
 //Variables a usar en todos lados
 const global = {
-    user_roles : ['admin', 'contador', 'auxiliar']
+    user_roles : ['admin', 'contador', 'auxiliar'],
+    niveles_cuenta : ['cuenta', 'grupo', 'subgrupo', 'mayor', 'analitica']
 }
 
 module.exports = global;
